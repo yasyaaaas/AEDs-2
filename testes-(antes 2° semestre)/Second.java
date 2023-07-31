8856f265245e81a7a8bbded614c1a493f9ad51a4
yasmin <130803911+yasyaaaas@users.noreply.github.com>
2023-07-31 10:30:36 -0300

    Add files via upload

---
+++ testes-(antes 2° semestre)/Second.java
@@ -0,0 +1,13 @@
+public class Second {
+    /*
+     * public static void main(String[] args) {
+     * Mya myObj = new Mya(); // puxa o 5 do arquivo Mya
+     * System.out.println(myObj.x); -> isso apara mostrar q em um arquivo vc
+     * consegue chamar coisas de outro arquivo java para esse
+     * }
+     */
+    public static void main(String[] args) {
+        // fazer exercícios dos slides de AEDS2 que estão no github
+
+    }
+}
