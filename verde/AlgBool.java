2302b0caa654206efa73c4e0d18d024e91eb7748
yasmin <130803911+yasyaaaas@users.noreply.github.com>
2023-09-10 20:06:16 -0300

    Add files via upload

---
+++ verde/AlgBool.java
@@ -0,0 +1,12 @@
+public class AlgBool {
+    public static void main(String[] args) {
+        String str;
+        while (true) {
+            str = MyIO.readLine();
+            if (str.charAt(0) == 0) {
+                break;
+            }
+
+        }
+    }
+}
